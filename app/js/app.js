1243b4ab2ee62b040b3505c368f123b50f470210
Tom Pickard <tcpickard94@gmail.com>
2016-03-11 11:18:01 +0000

    Completed user story 3 - View User
    Seperated views into partials
    Added NgRoute and routing logic to app.js
    Created a new controller module and another controller for individual
    user page
    Renamed listController to be more appropriate
    Feature tests written for above logic

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,23 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-var githubProfile = angular.module('githubProfile', [])
+var githubProfile = angular.module('githubProfile', [
+    'ngRoute',
+    'githubProfileControllers'
+]);
+
+githubProfile.config(['$routeProvider', 
+    function($routeProvider) {
+        $routeProvider.
+            when('/users', {
+                templateUrl: 'partials/user-list.html',
+                controller: 'GithubProfileCtrl'
+            }).
+            when('/users/:userLogin', {
+                templateUrl: 'partials/user-detail.html',
+                controller: 'UserDetailCtrl'
+            }).
+            otherwise({
+                redirectTo: '/users'
+            });
+}]);
--- app/js/controllers/listsController.js
+++
@@ -1,8 +0,0 @@
-githubProfile.controller('GithubProfileCtrl', ['$http', function($http) {
-  var self = this;
-  
-  $http.get('https://api.github.com/orgs/makersacademy/members').success(function(data) {
-    self.userData = data;
-  });
-
-}]);
